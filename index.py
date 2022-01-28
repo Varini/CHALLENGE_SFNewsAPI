e382a3472a1cc1efff64a13590e7fa95cb7b3a16
Leonardo <leonardovarini@gmail.com>
2022-01-27 22:44:52 -0300

    Index
    Setup server and scheduler job.

--- index.py
+++ index.py
@@ -1,12 +1,29 @@
 import uvicorn
+import warnings
 from fastapi import FastAPI
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from db import db_sync
 
 from routes.articles import app_article
+
+warnings.filterwarnings('ignore')
 
 app = FastAPI()
 
 app.include_router(app_article)
 
+app.on_event("startup")
+
+
+def init_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(id="db_sync", func=db_sync, trigger="cron",
+                      hour="20", minute="51", second="00")
+    scheduler.start()
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-    
+    init_scheduler()
+    uvicorn.run(app="index:app", host="0.0.0.0",
+                port=8000, reload=False, debug=False)
