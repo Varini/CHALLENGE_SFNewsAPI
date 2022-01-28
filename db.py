8467255cd5d824d344c611b2e65365fbe7fd83ce
Leonardo <leonardovarini@gmail.com>
2022-01-27 22:06:19 -0300

    Database configuration files
    Create a MongoDB cluster connection based on an environment file.

--- db.py
+++ db.py
@@ -1,7 +1,14 @@
+from bson import ObjectId
 from pymongo import MongoClient
+from dotenv import dotenv_values, load_dotenv
 
-client = MongoClient("<connection string>")
-db = client.sfnewsDB
-articles = db["articles"]
+import json
+import requests
+
+config = dotenv_values("D:\Git\CHALLENGE_SFNewsAPI_DEV\.env")
+
+conn = MongoClient(config['CONNECT_STR'])
+
+db = conn.sfnewsDB
 
 articles = db.articles
