25e175014b5004771c2e5540e04b95513fb32613
Leonardo <leonardovarini@gmail.com>
2022-01-27 22:36:21 -0300

    Model articles
    Alter class 'Article' and create class 'NewArticle'

--- models/articles.py
+++ models/articles.py
@@ -17,6 +17,17 @@ class PyObjectId(ObjectId):
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
 
+
+class Launches(BaseModel):
+    id: str = Field(...)
+    provider: str = Field(...)
+
+
+class Events(BaseModel):
+    id: int = Field(...)
+    provider: str = Field(...)
+
+
 class Article(BaseModel):
     _id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     id: Optional[int]
@@ -28,6 +39,8 @@ class Article(BaseModel):
     publishedAt: str = Field(...)
     updatedAt: str = Field(...)
     featured: bool = Field(...)
+    launches: Optional[List[Launches]] = []
+    events: Optional[List[Events]] = []
 
     class Config:
         allow_population_by_field_name = True
@@ -43,5 +56,58 @@ class Article(BaseModel):
                 "publishedAt": "12-21-2021",
                 "updatedAt": "01-01-2022",
                 "featured": False,
+                "launches": [
+                    {
+                        "id": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+                        "provider": "Provider Title"
+                    }
+                ],
+                "events": [
+                    {
+                        "id": 0,
+                        "provider": "Event Title"
+                    }
+                ],
+            }
+        }
+
+
+class NewArticle(BaseModel):
+    title: Optional[str]
+    url: Optional[str]
+    imageUrl: Optional[str]
+    newsSite: Optional[str]
+    summary: Optional[str]
+    publishedAt: Optional[str]
+    updatedAt: Optional[str]
+    featured: Optional[str]
+    launches: Optional[List[Launches]] = []
+    events: Optional[List[Events]] = []
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "title": "Article Title",
+                "url": "Article URL",
+                "imageUrl": "Article Image URL",
+                "newsSite": "News Site Name",
+                "summary": "",
+                "publishedAt": "12-21-2021",
+                "updatedAt": "01-01-2022",
+                "featured": False,
+                "launches": [
+                    {
+                        "id": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+                        "provider": "Provider Title"
+                    }
+                ],
+                "events": [
+                    {
+                        "id": 0,
+                        "provider": "Event Title"
+                    }
+                ],
             }
         }
